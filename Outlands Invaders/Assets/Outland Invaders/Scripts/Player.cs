bc0c2d91c3161c392f81fa53c1bd55bab12a4e1e
viniaf13 <viniaf13@gmail.com>
2020-07-31 12:37:34 -0300

    made particle system for lasers

---
+++ Outlands Invaders/Assets/Outland Invaders/Scripts/Player.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float speed = 20f;
+    [SerializeField] float xLimit = 9f;
+    [SerializeField] float yLimit = 8f;
+
+    [Header("Rotation")]
+    [SerializeField] float positionPitchFactor = -2f;
+    [SerializeField] float inputPitchFactor = -30f;
+    [SerializeField] float positionYawFactor = 2f;
+    [SerializeField] float inputRollFactor = -20f;
+
+    private float xInput;
+    private float yInput;
+
+    void Update()
+    {
+        Translate();
+        Rotate();
+    }
+
+    private void Translate()
+    {
+        xInput = Input.GetAxis("Horizontal");
+        yInput = Input.GetAxis("Vertical");
+
+        float xOffset = xInput * speed * Time.deltaTime;
+        float yOffset = yInput * speed * Time.deltaTime;
+
+        float clampedX = Mathf.Clamp(transform.localPosition.x + xOffset, -xLimit, xLimit);
+        float clampedY = Mathf.Clamp(transform.localPosition.y + yOffset, -yLimit, yLimit);
+
+        transform.localPosition = new Vector3 (clampedX, clampedY, transform.localPosition.z);
+    }
+    private void Rotate()
+    {
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToInput = yInput * inputPitchFactor;
+        float pitch = pitchDueToPosition + pitchDueToInput;
+
+        float yaw = transform.localPosition.x * positionYawFactor;
+        float roll = xInput * inputRollFactor; 
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+}
